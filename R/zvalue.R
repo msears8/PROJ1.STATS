f5729b482c0752a8dfd07197e6d4c33befba7343
Mya Sears <msears8@ou.edu>
2020-09-18 08:13:03 -0500

    New function for finding the s^2 value.

---
+++ R/zvalue.R
@@ -0,0 +1,12 @@
+#' @title A function for finding the z values.
+#'
+#' @param z The z values for a dataset
+#'
+#' @return The s^2 value
+#' @export
+#'
+#' @examples
+#'
+sdvalue=function(z){
+  (sd(z))^2
+}
